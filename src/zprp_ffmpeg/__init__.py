6d30b453fe0581d702f9403c45e52f3a102142bf
twezowic <01169258@pw.edu.pl>
2024-05-20 22:25:36 +0200

    feat: Add ffprobe function

--- src/zprp_ffmpeg/__init__.py
+++ src/zprp_ffmpeg/__init__.py
@@ -6,6 +6,7 @@ from ._api_compat import run
 from ._api_compat import run_async
 from .filters import hflip
 from .generated_filters import *  # noqa: F403 this is impossible to avoid
+from .probe import probe
 
 # This is for `from xyz import *`, but also to make linter shut up
-__all__ = ["input", "output", "run", "run_async", "hflip"]
+__all__ = ["input", "output", "run", "run_async", "hflip", "probe"]
