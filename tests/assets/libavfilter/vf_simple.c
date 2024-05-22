71a6f327f331986c710e11c243900f45cab42b6b
Tomasz S <madghostek@gmail.com>
2024-05-22 03:02:03 +0200

    test: switch around contents of test file for better coverage

--- tests/assets/libavfilter/vf_simple.c
+++ tests/assets/libavfilter/vf_simple.c
@@ -1,7 +1,7 @@
 #include "avfilter.h"
 #include "internal.h"
 
-static const AVFilterPad avfilter_simple_inputs[] = {
+static const AVFilterPad avfilter_simple_outputs[] = {
     {
         .name         = "default",
         .type         = AVMEDIA_TYPE_VIDEO,
@@ -14,8 +14,8 @@ const AVFilter ff_vf_simple = {
     .name          = "simple123",
     .description   = NULL_IF_CONFIG_SMALL("simple filter"),
     .priv_size     = 123,
-    FILTER_INPUTS(avfilter_simple_inputs),
-    FILTER_OUTPUTS(ff_video_default_filterpad),
+    FILTER_INPUTS(ff_video_default_filterpad),
+    FILTER_OUTPUTS(avfilter_simple_outputs),
     FILTER_QUERY_FUNC(query_formats),
     .flags         = AVFILTER_FLAG_SLICE_THREADS | AVFILTER_FLAG_SUPPORT_TIMELINE_GENERIC,
 };
