441afa256f87ddae89335e3114ac64a83315d1df
twezowic <01169258@pw.edu.pl>
2024-05-31 22:21:50 +0200

    Try fix the docs.

--- src/zprp_ffmpeg/__init__.py
+++ src/zprp_ffmpeg/__init__.py
@@ -6,18 +6,18 @@ from inspect import isfunction
 from zprp_ffmpeg import _api_compat
 from zprp_ffmpeg import generated_filters
 
-from ._api_compat import input
-from ._api_compat import output
-from ._api_compat import run
-from ._api_compat import run_async
+from ._api_compat import input  # noqa: F401
+from ._api_compat import output  # noqa: F401
+from ._api_compat import run  # noqa: F401
+from ._api_compat import run_async  # noqa: F401
 from .FilterGraph import Stream
-from .filters import hflip
+from .filters import hflip  # noqa: F401
 from .generated_filters import *  # noqa: F403 this is impossible to avoid
-from .probe import probe
-from .view import view
+from .probe import probe  # noqa: F401
+from .view import view  # noqa: F401
 
 # This is for `from xyz import *`, but also to make linter shut up
-__all__ = ["input", "output", "run", "run_async", "hflip", "probe", "view"]
+# __all__ = ["input", "output", "run", "run_async", "hflip", "probe", "view"]
 
 stream_modules = [generated_filters, _api_compat]
 
