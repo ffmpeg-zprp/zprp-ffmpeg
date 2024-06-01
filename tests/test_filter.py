55bd2be114646af29854207dbddac52ebac10521
b1rtek <53182944+B1rtek@users.noreply.github.com>
2024-06-01 20:10:18 +0200

    Adjusted tests

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -5,9 +5,11 @@ from zprp_ffmpeg.FilterGraph import FilterType
 
 def test_filter_build_command_no_opts():
     f = Filter("hflip", filter_type=FilterType.VIDEO.value)
-    assert f.get_command() == "-vf hflip"
+    sth = f.get_command()
+    assert f.get_command() == ":v]hflip"
 
 
 def test_filter_build_command_with_opts():
     f = Filter("scale", filter_type=FilterType.VIDEO.value, params=[FilterOption("w", "20"), FilterOption("h", "40")])
-    assert f.get_command() == "-vf scale=w=20:h=40"  # order isn't important in ffmpeg i think
+    sth = f.get_command()
+    assert f.get_command() == ":v]scale=w=20:h=40"  # order isn't important in ffmpeg i think
